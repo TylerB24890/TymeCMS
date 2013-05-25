4eecf84707c53810367ae71df1de691424e11c38
Tyler Bailey <tylerb.media@gmail.com>
2013-05-24 21:10:25 -0300

    Update database.php

--- application/config/development/database.php
+++ application/config/development/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'Redalert101';
-$db['default']['database'] = 'cms';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = FALSE;
